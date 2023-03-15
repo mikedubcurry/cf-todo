1590546175980df5916ea09c42db3a9bbc86d61b
mikedubcurry <michaelcurry95@gmail.com>
2023-03-15 19:34:10 -0400

    cloudflare adapter

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Todo Cloudflare</h1>
+<p>Visit <a href="https://developers.cloudflare.com">developers.cloudflare.com</a> to read the documentation</p>
